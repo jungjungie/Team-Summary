0d0f2ae321b41852158d237084fba16bdbb22894
Esther Min <estherjmin@gmail.com>
2020-06-30 20:42:52 -0400

    Created function to write file after exiting team summary prompts

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 // const path = require("path");
-// const fs = require("fs");
+const fs = require("fs");
 
 // const OUTPUT_DIR = path.resolve(__dirname, "output");
 // const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const render = require("./lib/htmlRenderer");
 
 const mgrQuestions = [
     {
@@ -96,8 +92,8 @@ const employees = [];
 
 async function createMgr() {
     // Asks for mgr info and creates new mgr based on user inputs
-    console.log('Please build your team:')
-    
+    console.log('\nPlease build your team:\n')
+
     const mgrData = await inquirer.prompt(mgrQuestions)
     let newMgr = new Manager(mgrData.name, mgrData.id, mgrData.email, mgrData.officeNumber);
 
@@ -128,7 +124,13 @@ async function addEmployees() {
 
     // Exits function
     } else {
-        console.log(employees);
+        // console.log(employees);
+
+        fs.writeFile('output/team.html', render(employees), function (err) {
+            if (err) throw err;
+            console.log('Success! Team.html was created in output folder.')
+        })
+        
         return;
     }
 }
@@ -144,10 +146,6 @@ createMgr();
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
